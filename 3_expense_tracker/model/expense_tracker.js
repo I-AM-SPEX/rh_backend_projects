3c617c361af74ddc7021d7f2eb46e068e107eb52
samuel williams <samuelspexy@gmail.com>
2024-09-15 20:54:39 +0200

    add functionality works

--- 3_expense_tracker/model/expense_tracker.js
+++ 3_expense_tracker/model/expense_tracker.js
@@ -1,4 +1,5 @@
-import  fs from 'fs'
+import fs from 'fs'
+import { escape } from 'querystring';
 
 const path = 'expenses.json'
 
@@ -7,13 +8,35 @@ export class ExpenseTracker {
 
     }
 
-   
+
     add(expense) {
-        if(fs.existsSync(path)) {
-            console.log('the file exists');
-        }else {
-            console.log('the file does not exists')
+        try {
+            if (fs.existsSync(path)) {
+                const data = fs.readFileSync(path);
+                const expenses = JSON.parse(data);
+                console.log(expenses)
+                expense.setId(expenses.expenses.length + 1);
+                expenses.expenses.push(expense);
+                console.log(expenses);
+
+                const json_string = JSON.stringify(expenses);
+                fs.writeFileSync(path, json_string);
+
+
+
+
+            } else {
+                const data = { expenses: [] };
+                expense.setId(data.expenses.length + 1);
+                data.expenses.push(expense);
+                const json_string = JSON.stringify(data);
+                fs.writeFileSync(path, json_string);
+
+            }
+        } catch (error) {
+            console.error(error)
         }
+
     }
 
     update() {
